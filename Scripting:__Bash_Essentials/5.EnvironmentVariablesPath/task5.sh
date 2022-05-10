b7424270539a1562f9e3913e9a152dc623c43a75
YourTheBestFriend <goncharovgithub8@gmail.com>
2022-05-10 13:45:06 +0000

    update 5.EnvironmentVariablesPath

--- Scripting:__Bash_Essentials/5.EnvironmentVariablesPath/task5.sh
+++ Scripting:__Bash_Essentials/5.EnvironmentVariablesPath/task5.sh
@@ -1,4 +1,5 @@
 #!/bin/bash 
+mkdir homework # create homework folder in your user's home folder
 SURNAME="Goncharov" # Create variable
 # Variable
 export SURNAME=$SURNAME # create SURNAME environment variable equal to your surname
